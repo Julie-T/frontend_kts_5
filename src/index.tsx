a45bd7e5e91cd1daa59ffe856ce72be42f78d77c
Juliya <yula_sakura@list.ru>
2023-03-12 13:33:41 +0300

    add_files

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import * as React from  'react';
+// import * as React from  'react';
 import {render} from 'react-dom';
-import styles from './styles.module.scss'
-import './global.scss'
-import Button from "components/Button";
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+import App from "../src/App";
+import "config/configureMobX";
 import 'regenerator-runtime';
 
-render(<div className={styles.title}>
-    React приложение
-    <div className={styles.picture}/>
-    <Button object={null}>Button</Button>
-</div>, document.getElementById('root'));
+render(
+//     <div className={styles.title}>
+//     React приложение
+//     <div className={styles.picture}/>
+// </div>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    , document.getElementById('root'));
 
 if (module.hot) {
     module.hot.accept();
---
+++ src/store/models/gitHub/gitHubRepoItemApi/index.ts
@@ -0,0 +1,1 @@
+export {gitHubRepoItemApi, gitHubRepoItemModel, dateOptionsType, DateType, normalizeGitHubRepoItem} from "./gitHubRepoItemApi";
